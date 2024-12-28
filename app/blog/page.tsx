ca755836542a333a841d937828b7b9d5b054975f
Ashikur Rahman <ashikur.jebu111@gmail.com>
2024-12-28 23:28:13 +0600

    changed home and blog page banner

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -13,6 +13,7 @@ import React from "react";
 import PostList from "../components/posts/PostList";
 import { getAllPosts } from "@/quries/blog-data";
 import { Edge } from "@/types";
+import style from "./style.module.css";
 
 type PageInfoType = {
   hasNextPage: boolean;
@@ -32,7 +33,21 @@ const BlogPage = async ({
   const endCursor = searchParams.endCursor;
   const posts = await getAllPosts(null, endCursor);
 
-  return <PostList posts={posts?.edges} pageInfo={posts.pageInfo} />;
+  return (
+    <section className="">
+      <div className="banner-section-blog z-[1] overflow-hidden relative w-full h-[50vh] bg-[#222434] flex items-center justify-center">
+        <h1 className="text-5xl font-bold text-white capitalize z-10">
+          Here are all the blogs
+        </h1>
+        <button className="nav-link d-center box-style btn-box p-0 false">
+          See All
+        </button>
+      </div>
+      <div>
+        <PostList posts={posts?.edges} pageInfo={posts.pageInfo} />
+      </div>
+    </section>
+  );
 };
 
 export default BlogPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,33 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex relative flex-col justify-center items-center h-screen">
-      <div className="p-2 flex flex-col justify-center items-center bg-gray-100 lg:w-2/5 h-1/4 rounded">
-        <div className="flex pb-5 mb-5 text-5xl text-back">
-          <h2>BlogCast</h2>
+    <section className="banner-section relative bg-[#171744]">
+      <div className="flex relative flex-col justify-center items-center h-screen">
+        <div className="p-2 flex flex-col justify-center items-center bg-gray-100/5 lg:w-2/5 h-1/4 rounded">
+          <div className="flex pb-5 mb-5 text-5xl text-white">
+            <h2>BlogCast</h2>
+          </div>
+          <div>
+            <Link
+              href="/blog"
+              className="bg-black text-white hover:bg-black/90 p-3 rounded-sm text-lg text mr-2"
+            >
+              TAKE ME TO THE BLOG
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link
-            href="/blog"
-            className="bg-black text-white hover:bg-black/90 p-3 rounded-sm text-lg text mr-2"
-          >
-            TAKE ME TO THE BLOG
-          </Link>
+        <div className="mt-10">
+          <Image
+            className="scroll-thumb"
+            src={"/scroll.png"}
+            alt="scroll"
+            width={35}
+            height={35}
+          />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
